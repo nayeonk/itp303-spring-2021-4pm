ed61752b2a745bccdb5c4f829b129c893155e14d
Nayeon Kim <nykim92@gmail.com>
2021-04-21 20:58:12 -0700

    Finish off ajax and php lecture

--- lect22-ajax-php/backend.php
+++ lect22-ajax-php/backend.php
@@ -16,29 +16,54 @@
 	// can't echo out an assoc array
 	// echo $php_array;
 
+	//$_GET["searchTerm"];
+
 	// convert associative array into a JSON string
-	echo json_encode($php_array);
+	//echo json_encode($php_array);
 
-	// $host = "303.itpwebdev.com";
-	// $user = "";
-	// $pass = "";
-	// $db = "";
+	// Connect to the database to search for a song in the database
+	$host = "303.itpwebdev.com";
+	$user = "nayeon_db_user";
+	$pass = "uscItp2021!";
+	$db = "nayeon_song_db";
 
-	// $mysqli = new mysqli($host, $user, $pass, $db);
-	// if ( $mysqli->errno ) {
-	// 	echo $mysqli->error;
-	// 	exit();
-	// }
+	$mysqli = new mysqli($host, $user, $pass, $db);
+	if ( $mysqli->errno ) {
+		echo $mysqli->error;
+		exit();
+	}
 
-	// $sql = "";
+	// Generate the SQL based on the user's input
+	$sql = "SELECT * FROM tracks WHERE name LIKE '%" . $_GET["term"] . "%' LIMIT 10;";
 
-	// $results = $mysqli->query($sql);
-	// if ( !$results ) {
-	// 	echo $mysqli->error;
-	// 	exit();
-	// }
+	$results = $mysqli->query($sql);
+	if ( !$results ) {
+		echo $mysqli->error;
+		exit();
+	}
 
-	// $mysqli->close();
+	$mysqli->close();
 
+	$results_array = [];
+
+	// Run the while loop to get all the results. Store the results in another variable
+	while( $row = $results->fetch_assoc()) {
+		array_push($results_array, $row);
+	}
+
+	// Convert the assoc array to a json string
+	echo json_encode($results_array);
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
